54709522dae28cd5eee6bfa9324c257dfb17b88a
Samuel Weisse <samuel.weisse@gmail.com>
2016-06-08 10:02:21 -0700

    working on drag and drop

--- game/index.js
+++ game/index.js
@@ -11,9 +11,32 @@ $( window ).load(function() {
 	$($start).click(function(){
 		$('.token').removeClass('hide');
 		$start.removeClass('start').addClass('reset').html('Reset');
+		$tokenMove();
 		$resetButton();
 	})
 });
+
+var $tokenMove = function(){
+	// http://javascript.info/tutorial/mouse-events
+	$('.token').mousedown(function() {
+		$(this).css('position','absolute')
+		var self = $(this);
+		console.log(self);
+
+		$(document).mousemove(function(event){
+			var X = event.pageX;
+			var Y = event.pageY;
+			self.style.left = event.pageX-25+'px';
+    		self.style.top = event.pageY-25+'px';
+
+		});
+	// 	$(this).mouseup(function() {
+ //    		document.onmousemove = null
+ //  		});
+	});
+	// document.getElementsByClassName('token').ondragstart = function() { return false }
+
+}
 
 var $resetButton = function(){
 	$($start).click(function(){
