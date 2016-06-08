0c19f2200321aff4ffd451ce7e2b1b90277167cb
Samuel Weisse <samuel.weisse@gmail.com>
2016-06-07 18:02:24 -0700

    working start & reset buttons

--- game/index.js
+++ game/index.js
@@ -1,1 +1,22 @@
 console.log('Initiation complete. Hello')
+
+var $start = $('<button>').addClass('start').html('Start');
+
+
+
+$( window ).load(function() {
+	$('.token').addClass('hide');
+	$(".board").removeClass('hide');
+	$('h1').append($start);
+	$($start).click(function(){
+		$('.token').removeClass('hide');
+		$start.removeClass('start').addClass('reset').html('Reset');
+		$resetButton();
+	})
+});
+
+var $resetButton = function(){
+	$($start).click(function(){
+		location.reload();
+	});
+}
